31cec38ee3ef0f67a7ad530e4d6e621635a28d1e
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-08-25 02:47:24 +0300

    Add Legal Page LInk

---
+++ src/app/(legal)/cookie-policy/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+
+export default function CookiePolicy(){
+    useEffect(() => {
+        // Create a script element
+        const script = document.createElement('script');
+        script.src = "https://cdn.iubenda.com/iubenda.js";
+        script.async = true;
+
+        // Append the script to the body
+        document.body.appendChild(script);
+
+        // Cleanup function to remove the script if component unmounts
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []); // Empty dependency array means this effect runs once after initial render
+
+    return (
+        <Link
+            href="https://www.iubenda.com/privacy-policy/66775185/cookie-policy"
+            title="Privacy Policy"
+        >
+            Cookie Policy
+        </Link>
+    );
+};
+
+
