63c94eb2ab46e58c32ec48b1d08a9162e9785ada
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-09-07 13:25:46 +0300

    Fix Error

--- src/components/specific/Navbar.tsx
+++ src/components/specific/Navbar.tsx
@@ -3,14 +3,14 @@ import { ThemeToggle } from '@/components/specific/ThemeToggle';
 import { Button } from '../ui/button';
 import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import Image from 'next/image';
-import logo from "$/public/assets/logo.png"
+// import logo from "$/public/assets/logo.png"
 export async function Navbar() {
 
     return (
         <nav className="z-50 bg-background h-[10vh] flex items-center sticky top-0 inset-x-0">
             <div className="container flex items-center justify-between gap-x-4">
                 <Link href="/">
-                    <Image src={logo} width={100} alt='WeGo profile Image' className='w-full h-5' priority />
+                    <Image src='../../../public/assets/logo.PNG' width={100} alt='WeGo profile Image' className='w-full h-5' priority />
                 </Link>
                 <div className="flex items-center gap-x-3">
                     <ThemeToggle />
